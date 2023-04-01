89680ecb46e8219b18a808e3b4cebdd9b6cdbdb7
razenkov <razenkov.rna@mail.ru>
2023-04-01 18:40:58 +0300

    1 часть

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,28 @@
-public class Application {
+import java.sql.*;
 
+public class Application {
+    public static void main(String[] args) throws SQLException {
+        final String user = "postgres";
+        final String password = "Zxc13579";
+        final String url = "jdbc:postgresql://localhost:5432/skypro";
+
+        try (final Connection connection = DriverManager.getConnection((url), user, password);
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM employee WHERE id = (?)")) {
+            statement.setInt(1, 3);
+            final ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String name = "Имя: " + resultSet.getString("first_name");
+                String lastName = "Фамилия: " + resultSet.getString("last_name");
+                String gender = "Пол: " + resultSet.getString("gender");
+                int city_id = resultSet.getInt("city_id");
+
+                System.out.println(name);
+                System.out.println(lastName);
+                System.out.println(gender);
+                System.out.println("id города: " + city_id);
+            }
+
+        }
+    }
 }
+
